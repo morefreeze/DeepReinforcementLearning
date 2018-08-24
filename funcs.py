1936f8df83248a778da0a6035aa6d574ade8c493
morefreeze <morefreeze@gmail.com>
2018-08-24 14:48:18 +0800

    Add missing stuff for running

--- funcs.py
+++ funcs.py
@@ -10,8 +10,9 @@ from agent import Agent, User
 
 import config
 
+# todo: multi-player supported
 def playMatchesBetweenVersions(env, run_version, player1version, player2version, EPISODES, logger, turns_until_tau0, goes_first = 0):
-    
+
     if player1version == -1:
         player1 = User('player1', env.state_size, env.action_size)
     else:
@@ -26,7 +27,6 @@ def playMatchesBetweenVersions(env, run_version, player1version, player2version,
         player2 = User('player2', env.state_size, env.action_size)
     else:
         player2_NN = Residual_CNN(config.REG_CONST, config.LEARNING_RATE, env.input_shape,   env.action_size, config.HIDDEN_CNN_LAYERS)
-        
         if player2version > 0:
             player2_network = player2_NN.read(env.name, run_version, player2version)
             player2_NN.model.set_weights(player2_network.get_weights())
@@ -37,7 +37,11 @@ def playMatchesBetweenVersions(env, run_version, player1version, player2version,
     return (scores, memory, points, sp_scores)
 
 
-def playMatches(player1, player2, EPISODES, logger, turns_until_tau0, memory = None, goes_first = 0):
+BEST_PLAYER_ID = 0
+PLAYER_ID1 = 1
+PLAYER_ID2 = 2
+PLAYER_ID3 = 3
+def playMatches(player1, player2, EPISODES, logger, turns_until_tau0, memory = None, goes_first = -1):
 
     env = Game()
     scores = {player1.name:0, "drawn": 0, player2.name:0}
@@ -53,34 +57,32 @@ def playMatches(player1, player2, EPISODES, logger, turns_until_tau0, memory = N
         print (str(e+1) + ' ', end='')
 
         state = env.reset()
-        
+
         done = 0
         turn = 0
         player1.mcts = None
         player2.mcts = None
 
-        if goes_first == 0:
-            player1Starts = random.randint(0,1) * 2 - 1
+        if goes_first == -1:
+            player1Starts = random.randint(0,1)  # todo: player number
         else:
             player1Starts = goes_first
 
-        if player1Starts == 1:
-            players = {1:{"agent": player1, "name":player1.name}
-                    , -1: {"agent": player2, "name":player2.name}
+        if player1Starts == 0:
+            players = {BEST_PLAYER_ID:{"agent": player1, "name":player1.name}
+                    , PLAYER_ID1: {"agent": player2, "name":player2.name}
                     }
-            logger.info(player1.name + ' plays as X')
         else:
-            players = {1:{"agent": player2, "name":player2.name}
-                    , -1: {"agent": player1, "name":player1.name}
+            players = {BEST_PLAYER_ID:{"agent": player2, "name":player2.name}
+                    , PLAYER_ID1: {"agent": player1, "name":player1.name}
                     }
-            logger.info(player2.name + ' plays as X')
-            logger.info('--------------')
+        logger.info('--------------')
 
         env.gameState.render(logger)
 
         while done == 0:
             turn = turn + 1
-    
+
             #### Run the MCTS algo and return an action
             if turn < turns_until_tau0:
                 action, pi, MCTS_value, NN_value = players[state.playerTurn]['agent'].act(state, 1)
@@ -100,45 +102,28 @@ def playMatches(player1, player2, EPISODES, logger, turns_until_tau0, memory = N
             logger.info('====================')
 
             ### Do the action
-            state, value, done, _ = env.step(action) #the value of the newState from the POV of the new playerTurn i.e. -1 if the previous player played a winning move
-            
+            state, winner, done, _ = env.step(action) #the value of the newState from the POV of the new playerTurn i.e. -1 if the previous player played a winning move
+
             env.gameState.render(logger)
 
-            if done == 1: 
+            if done == 1:
                 if memory != None:
                     #### If the game is finished, assign the values correctly to the game moves
                     for move in memory.stmemory:
                         if move['playerTurn'] == state.playerTurn:
-                            move['value'] = value
-                        else:
-                            move['value'] = -value
-                         
+                            move['value'] = 1 if winner == state.playerTurn else -1
+
                     memory.commit_ltmemory()
-             
-                if value == 1:
-                    logger.info('%s WINS!', players[state.playerTurn]['name'])
-                    scores[players[state.playerTurn]['name']] = scores[players[state.playerTurn]['name']] + 1
-                    if state.playerTurn == 1: 
-                        sp_scores['sp'] = sp_scores['sp'] + 1
-                    else:
-                        sp_scores['nsp'] = sp_scores['nsp'] + 1
 
-                elif value == -1:
-                    logger.info('%s WINS!', players[-state.playerTurn]['name'])
-                    scores[players[-state.playerTurn]['name']] = scores[players[-state.playerTurn]['name']] + 1
-               
-                    if state.playerTurn == 1: 
-                        sp_scores['nsp'] = sp_scores['nsp'] + 1
-                    else:
-                        sp_scores['sp'] = sp_scores['sp'] + 1
-
+                logger.info('%s WINS!', players[winner]['name'])
+                scores[players[winner]['name']] += 1
+                if winner == BEST_PLAYER_ID:
+                    sp_scores['sp'] = sp_scores['sp'] + 1
                 else:
-                    logger.info('DRAW...')
-                    scores['drawn'] = scores['drawn'] + 1
-                    sp_scores['drawn'] = sp_scores['drawn'] + 1
+                    sp_scores['nsp'] = sp_scores['nsp'] + 1
 
                 pts = state.score
-                points[players[state.playerTurn]['name']].append(pts[0])
-                points[players[-state.playerTurn]['name']].append(pts[1])
+                for i in range(len(pts)):
+                    points[players[i]['name']].append(pts[i])
 
     return (scores, memory, points, sp_scores)
--- games/splendor/game.py
+++ games/splendor/game.py
@@ -2,6 +2,7 @@
 import random
 import itertools
 from enum import Enum, auto
+import numpy as np
 
 MAX_STONES = 10
 
@@ -15,7 +16,7 @@ class Color(Enum):
     BLACK = 5
     ALL = [WHITE, BLUE, GREEN, RED, BLACK]
 
-class GameElement(Enum):
+class PlayerElement(Enum):
     STONE = 0
     CARD = 1
     NOBLE = 2
@@ -32,6 +33,8 @@ class Position(Enum):
     PLAYER_POS = 7
     HALL = 8  # Noble stay here if they are present
     STONE = 9  # Stones stay here if they are present
+    CARDS = 10
+    NOBLES = 11
 
 class ActionType(Enum):
     PICK_STONES = auto()
@@ -83,11 +86,13 @@ class Stone(object):
         return '%s(%s)' % (self.__class__.__name__, self)
 
 class Card(Stone):
+    id = None
     score = 0
     # List of Stone
     need = Stones()
 
-    def __init__(self, color, need, score=0):
+    def __init__(self, id, color, need, score=0):
+        self.id = id
         self.color = color
         if type(need) == dict:
             need = Stones(need)
@@ -95,7 +100,7 @@ class Card(Stone):
         self.score = score
 
     def __str__(self):
-        return '%s->%s %s' % ({st: num for st, num in self.need.items() if num > 0}, self.color, self.score)
+        return 'id:%s %s->%s %s' % (self.id, {st: num for st, num in self.need.items() if num > 0}, self.color, self.score)
 
     def __repr__(self):
         return '%s(%s)' % (self.__class__.__name__, self)
@@ -151,6 +156,7 @@ class Cards(dict):
             ((0, 0, 0, 0, 7), 4),
             ((0, 3, 3, 5, 3), 3),
         ]
+        id_ = 0
         for deck, patterns in [
             (Position.DECK1, deck1_patterns),
             (Position.DECK2, deck2_patterns),
@@ -160,20 +166,23 @@ class Cards(dict):
             for pat, score in patterns:
                 for i in range(5):
                     self[deck].append(
-                        Card(Color(Color.ALL.value[i]),
+                        Card(id_, Color(Color.ALL.value[i]),
                              dict(zip(map(Color, (Color.ALL.value*2)[i:i+5]), pat)),
                             score)
                     )
+                    id_ += 1
 
 class CardStone(Stone):
     '''CardStone is a collections of cards, but it refer Stone attribute(color, num)'''
 
 class Noble(object):
+    id = None
     score = 0
     # List of CardType
     need = Stones()
 
-    def __init__(self, need, score=3):
+    def __init__(self, id_, need, score=3):
+        self.id = id_
         if type(need) == dict:
             need = Stones(need)
         self.need = need
@@ -194,29 +203,49 @@ class Noble(object):
 class Nobles(list):
     '''List all noble cards'''
     def __init__(self):
+        id_ = 0
         for i in range(5):
-            self.append(Noble(dict(zip(map(Color, (Color.ALL.value*2)[i:i+3]), [3,3,3])), score=4))
+            self.append(Noble(id, dict(zip(map(Color, (Color.ALL.value*2)[i:i+3]), [3,3,3])), score=4))
+            id_ += 1
         for i in range(5):
-            self.append(Noble(dict(zip(map(Color, (Color.ALL.value*2)[i:i+2]), [4,4])), score=3))
+            self.append(Noble(id, dict(zip(map(Color, (Color.ALL.value*2)[i:i+2]), [4,4])), score=3))
+            id_ += 1
 
 class Player(dict):
 
     def __init__(self):
         super().__init__()
-        self[GameElement.STONE] = Stones()
-        self[GameElement.CARD] = []
-        self[GameElement.NOBLE] = []
-        self[GameElement.FOLD] = []
+        self[PlayerElement.STONE] = Stones()
+        self[PlayerElement.CARD] = []
+        self[PlayerElement.NOBLE] = []
+        self[PlayerElement.FOLD] = []
+        self._score = None
 
     def win(self):
         return self.score >= 15
 
     @property
     def score(self):
+        if self._score is not None:
+            return self._score
         score = 0
-        for ele in (GameElement.CARD, GameElement.NOBLE):
+        for ele in (PlayerElement.CARD, PlayerElement.NOBLE):
             score += sum([c.score for c in self[ele]])
+        self._score = score
         return score
+
+    def binary(self, b):
+        cards_position = np.zeros(len(b[Position.CARDS]), dtype=np.int)
+        for c in self[PlayerElement.CARD]:
+            cards_position[c.id] = 1
+        for c in self[PlayerElement.FOLD]:
+            cards_position[c.id] = 1
+        nobles_position = np.zeros(len(b[Position.NOBLES]), dtype=np.int)
+        for n in self[PlayerElement.NOBLE]:
+            nobles_position[n.id] = 1
+        stones_position = np.array([x[1] for x in sorted(self[PlayerElement.STONE].items(), key=lambda x:x[0].value)]
+                                   , dtype=np.int)
+        return np.concatenate((cards_position, nobles_position, stones_position))
 
 class Board(dict):
 
@@ -230,6 +259,7 @@ class Board(dict):
         }
         self[Position.STONE] = init_stones[player_num]
         cards = Cards()
+        self[Position.CARDS] = sum(map(list, cards.values()), [])
         self[Position.DECK1] = random.sample(cards[Position.DECK1], len(cards[Position.DECK1]))
         self[Position.DECK2] = random.sample(cards[Position.DECK2], len(cards[Position.DECK2]))
         self[Position.DECK3] = random.sample(cards[Position.DECK3], len(cards[Position.DECK3]))
@@ -240,6 +270,7 @@ class Board(dict):
         self[Position.LINE3], self[Position.DECK3] = self[Position.DECK3][:draw_num], self[Position.DECK3][draw_num:]
 
         nobles = Nobles()
+        self[Position.NOBLES] = nobles
         random.shuffle(nobles)
         # Draw player_num+1 noble cards
         self[Position.HALL] = nobles[:player_num+1]
@@ -310,7 +341,7 @@ class PickStones(Action):
         stones = self.card_or_stone
         for c, num in stones.items():
             board[Position.STONE][c] -= num
-            player[GameElement.STONE][c] += num
+            player[PlayerElement.STONE][c] += num
 
 class NotFulfillError(Exception):
     pass
@@ -324,20 +355,20 @@ class PickCard(Action):
         player = board[Position.PLAYER_POS][self.playerTurn]
         card = self.card_or_stone
         in_line = any([card in board[line] for line in [Position.LINE1, Position.LINE2, Position.LINE3]])
-        return in_line and card.fulfill(player[GameElement.STONE], player[GameElement.CARD])[0]
+        return in_line and card.fulfill(player[PlayerElement.STONE], player[PlayerElement.CARD])[0]
 
     def apply(self, board):
         player = board[Position.PLAYER_POS][self.playerTurn]
         card = self.card_or_stone
-        is_fulfill, removed_stones = card.fulfill(player[GameElement.STONE], player[GameElement.CARD])
+        is_fulfill, removed_stones = card.fulfill(player[PlayerElement.STONE], player[PlayerElement.CARD])
         if not is_fulfill:
             raise NotFulfillError
-        player[GameElement.STONE] -= removed_stones
-        player[GameElement.CARD].append(card)
+        player[PlayerElement.STONE] -= removed_stones
+        player[PlayerElement.CARD].append(card)
         board.draw(card)
         # If fulfill noble then acquire automatically
         for noble in board[Position.HALL]:
-            if noble.fulfill(player[GameElement.CARD]):
+            if noble.fulfill(player[PlayerElement.CARD]):
                 pn = PickNoble(self.playerTurn, ActionType.PICK_NOBLE, noble)
                 pn.apply(b)
 
@@ -349,14 +380,14 @@ class PickNoble(Action):
     def is_playable(self, board):
         player = board[Position.PLAYER_POS][self.playerTurn]
         noble = self.card_or_stone
-        return noble in board[Positio.HALL] and noble.fulfill(player[GameElement.CARD])
+        return noble in board[Positio.HALL] and noble.fulfill(player[PlayerElement.CARD])
 
     def apply(self, board):
         if self.is_playable(board):
             player = board[Position.PLAYER_POS][self.playerTurn]
             noble = self.card_or_stone
             board[Position.HALL].remove(noble)
-            player[GameElement.NOBLE].append(noble)
+            player[PlayerElement.NOBLE].append(noble)
 
 class FoldCard(Action):
 
@@ -368,17 +399,17 @@ class FoldCard(Action):
         player = board[Position.PLAYER_POS][self.playerTurn]
         card = self.card_or_stone
         in_line = any([card in board[line] for line in [Position.LINE1, Position.LINE2, Position.LINE3]])
-        return in_line and len(player[GameElement.FOLD]) < 3
+        return in_line and len(player[PlayerElement.FOLD]) < 3
 
     def apply(self, board):
         if not self.is_playable(board):
             raise NotFulfillError
         player = board[Position.PLAYER_POS][self.playerTurn]
         card = self.card_or_stone
-        player[GameElement.FOLD].append(card)
+        player[PlayerElement.FOLD].append(card)
         if board[Position.STONE][Color.GOLD] > 0:
             board[Position.STONE][Color.GOLD] -= 1
-            player[GameElement.STONE][Color.GOLD] += 1
+            player[PlayerElement.STONE][Color.GOLD] += 1
         board.draw(card)
 
 class Game(object):
@@ -424,15 +455,25 @@ class GameState(object):
         self.id = self._convertStateToId()
         self.allowedActions = self._allowedActions()
         self.fairTurn = False
-        self.isEndGame = self._checkForEndGame()
-        self.value = self._getValue()
+        self.winner, self.isEndGame = self._checkForEndGame()
+        # self.value = self._getValue()
         self.score = self._getScore()
 
     def _binary(self):
-        return tuple(map(str, [self.board, self.players, self.playerTurn]))
+        '''First current player array, then player1(if is not current player) and so on'''
+        positions = np.array([], dtype=np.int)
+        positions = np.append(positions, self.players[self.playerTurn].binary(self.board))
+        for idx, p in enumerate(self.players):
+            if self.playerTurn != idx:
+                positions = np.append(positions, p.binary(self.board))
+        return positions
+
 
     def _convertStateToId(self):
-        return '|'.join(self.binary)
+        positions = np.array([], dtype=np.int)
+        for p in self.players:
+            positions = np.append(positions, p.binary(self.board))
+        return ','.join(map(str, positions))
 
     def _allowedActions(self):
         allowed_actions = []
@@ -460,28 +501,32 @@ class GameState(object):
         return allowed_actions
 
     def _checkForEndGame(self):
-        for p in self.board[Position.PLAYER_POS]:
+        players = self.board[Position.PLAYER_POS]
+        for p in players:
             if p.win():
                 self.fairTurn = True
-        if self.fairTurn and self.playerTurn == len(self.board[Position.PLAYER_POS]) - 1:
-            return 1
-        return 0
+        if self.fairTurn and self.playerTurn == len(players) - 1:
+            player_score = [(idx, p.score) for idx, p in enumerate(players)]
+            sorted(player_score, key=lambda x: (-x[0], x[1]))
+            return (player_score[0][0], 1)
+        return (0, 0)
 
     def _getValue(self):
-        # todo: sum all player score, if someone wins, return (-1, -1, 1) else (0, 0, 0)
-        # Maybe return score directly?
         return (0, 0, 0)
 
     def _getScore(self):
-        tmp = self.value
-        return (tmp[1], tmp[2])
+        # sum all player's score
+        return tuple(p.score for p in self.board[Position.PLAYER_POS])
 
     def takeAction(self, action):
+        '''new_state, value, done
+        winner: 0-4 player id
+        done: 1 for ending game 0 for otherwise'''
         action.apply(self.board)
         self.playerTurn = (self.playerTurn + 1) % len(self.players)
         new_state = GameState(self.board, self.playerTurn)
-        value, done = (new_state.value[0], 1) if new_state.isEndGame else (0, 0)
-        return new_state, value, done
+        winner, done = (new_state.winner, 1) if new_state.isEndGame else (0, 0)
+        return new_state, winner, done
 
     def render(self, logger):
         logger.info('-'*20)
@@ -490,11 +535,13 @@ if __name__ == '__main__':
     g = Game(player_num=4, seed=0)
     b = g.board
     gs = g.gameState
-    g.step(gs.allowedActions[-1])
+    new_gs, value, done, info = g.step(gs.allowedActions[-1])
+    print(new_gs.binary)
+    print(new_gs.id)
 
     p = g.player
     for _ in range(5):
         c = b[Position.LINE3][0]
-        p[GameElement.CARD].append(c)
+        p[PlayerElement.CARD].append(c)
         b.draw(c)
     print(p.score)
