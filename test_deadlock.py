dc876e88a629de9ca4467398a8e65f79b3f5df54
morefreeze <morefreeze@gmail.com>
2018-11-01 11:12:37 +0800

    Change test_deadlock to multiprocessing

--- test_deadlock.py
+++ test_deadlock.py
@@ -1,23 +1,27 @@
 '''Test is there a deadlock'''
-from tqdm import tqdm
+import multiprocessing
 from game import *
 
+def f():
+    seed = random.randrange(sys.maxsize)
+    g = Game(player_num=2, seed=seed)
+    gs = g.gameState
+    b = g.board
+    try:
+        for i in range(300):
+            action = random.choice(gs.allowedActions)
+            gs, value, done, info = g.step(action)
+            if done == 1:
+                break
+        else:
+            print("Sth must be deadlock {}".format(seed))
+            raise RuntimeError
+    except RuntimeError as e:
+        raise e
+    except:
+        pass
+
 if __name__ == '__main__':
-    for _ in tqdm(range(1000)):
-        seed = random.randrange(sys.maxsize)
-        g = Game(player_num=2, seed=seed)
-        gs = g.gameState
-        b = g.board
-        try:
-            for i in range(300):
-                action = random.choice(gs.allowedActions)
-                gs, value, done, info = g.step(action)
-                if done == 1:
-                    break
-            else:
-                print("Sth must be deadlock {}".format(seed))
-                raise RuntimeError
-        except RuntimeError as e:
-            raise e
-        except:
-            continue
+    with multiprocessing.Pool(5) as p:
+        res = [p.apply_async(f) for _ in range(100)]
+        list(map(lambda x: x.get(), res))
